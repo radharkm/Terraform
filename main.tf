45008c1d0f82cca640c5b97903b3a3bc02ac725c
radharkm <55471206+radharkm@users.noreply.github.com>
2023-08-05 22:26:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,40 +1,15 @@
 resource "aws_instance" "cloudcasts_web" {
-  ami           = var.instance_ami
-  instance_type = var.instance_size
+  ami           = "ami-00970f57473724c10"
+  instance_type = "t2.micro"
  
   root_block_device {
-    volume_size = var.instance_root_device_size
+    volume_size = "1 Gib"
     volume_type = "gp3"
   }
  
   tags = {
-    Name        = "cloudcasts-${var.infra_env}-web"
-    Role        = var.infra_role
-    Project     = "cloudcasts.io"
-    Environment = var.infra_env
+    Name        = " ec2-testing"
     ManagedBy   = "terraform"
   }
 }
  
-resource "aws_eip" "cloudcasts_addr" {
-  # We're not doing this directly
-  # instance = aws_instance.cloudcasts_web.id
-  vpc      = true
- 
-  lifecycle {
-    prevent_destroy = true
-  }
- 
-  tags = {
-    Name        = "cloudcasts-${var.infra_env}-web-address"
-    Role        = var.infra_role
-    Project     = "cloudcasts.io"
-    Environment = var.infra_env
-    ManagedBy   = "terraform"
-  }
-}
- 
-resource "aws_eip_association" "eip_assoc" {
-  instance_id   = aws_instance.cloudcasts_web.id
-  allocation_id = aws_eip.cloudcasts_addr.id
-}
